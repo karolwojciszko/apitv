08e3f15ae5ff16e03e4c76bc32f1797c9f90f4a8
Karol Wojciszko <karol.wojciszko@gmail.com>
2016-11-08 17:36:32 +0100

    v1.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   #get 'tvguide/get'
-  get 'tvguide/get' #,defaults: { start: Time.now }
+  root get 'tvguide/get' #,defaults: { start: Time.now }
 
-  get 'welcome/index'
+  #get 'welcome/index'
  
-  root 'welcome#index'
+  #root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
