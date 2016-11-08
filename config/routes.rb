f9f3db6230f68a0593e5d789828736bc0d37d741
Karol Wojciszko <karol.wojciszko@gmail.com>
2016-11-08 17:48:56 +0100

    v1.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'tvguide/get'
 
   get 'welcome/index'
-  root get 'tvguide#get'
+  root 'tvguide#get'
   #root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
